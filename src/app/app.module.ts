6f6b66e3be8588ca6f6e889e15ea89d99ac3752c
ldsls <18052776103@163.com>
2019-12-09 00:55:19 +0800

    add ant design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,14 @@ import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
 import { MainFunctionModule } from './main-function/main-function.module';
 import { NewProjectModule } from './new-project/new-project.module';
+import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [
@@ -17,9 +25,13 @@ import { NewProjectModule } from './new-project/new-project.module';
     AppRoutingModule,
     LoginModule,
     MainFunctionModule,
-    NewProjectModule
+    NewProjectModule,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
